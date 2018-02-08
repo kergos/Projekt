ec49b774c6ebbece71c9c20eec2901b14d341941
Manuel Matzinger <manuel.matzinger@hotmail.com>
2018-02-08 16:35:05 +0100

    Basic Plotting from File

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,5 @@
 from src.functions import *
 
-print("Plotting?")
-
-fileplotfunction()
-
+#basicfileplotfunction()
+#filesystemplotfunction() #only use when ready
+fileplotfunction("../../Batchelor-Arbeit/Messdaten/T27C/A2/MoS2_encapsulated_DevA2_IdVg_10Vs_Vd05V_27C_-20_20V.crv")
--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import os.path
+import os
+import glob
+from pathlib import Path
 
 
 def basicplotfunction():
@@ -23,7 +25,7 @@ def basicplotfunction():
     plt.show()
 
 
-def fileplotfunction():
+def basicfileplotfunction():
     with open("../../Batchelor-Arbeit/Messdaten/T27C/A1/MoS2_encapsulated_DevA1_IdVg_10Vs_Vd1V_27C_-20_20V.crv") as f:
         for i in range(6):      # starting at line 11 where plot data starts
             f.__next__()
@@ -61,5 +63,71 @@ def fileplotfunction():
     ax.grid()
     fig.savefig("../../Batchelor-Arbeit/Plots/T27C/A1_IDrain.png")
 
+def fileplotfunction(pathtofile):
+    if os.path.isfile(pathtofile):
+        with open(pathtofile) as f:
+            for i in range(6):      # starting at line 11 where plot data starts
+                f.__next__()
+            data = f.read()
+
+        data = data.split('\n') #splitting seperate lines
+        #print(data)
+        data = [row.split('  ') for row in data] #removing blanks
+        data.pop() #remove last useless Array
+        x  = [float(column[1]) for column in data]
+        y1 = [float(column[2]) for column in data]
+        y2 = [float(column[3]) for column in data]
+        y3 = [float(column[4]) for column in data]
+
+        #string manipulation for naming purposes
+        newstring = pathtofile.split('/')
+        print(newstring)
+        dirname1 = newstring[4]
+        dirname2 = newstring[5]
+        filename = newstring[6]
+        print( dirname1, dirname2 , filename[:-4])
+        """
+
+        fig, ax = plt.subplots()
+        ax.plot(x, y1)
+        ax.set(xlabel='Time (s)', ylabel='VBackGate (V)',
+               title='T27C-A1_VBackGate')
+        ax.grid()
+        fig.savefig("../../Batchelor-Arbeit/Plots/T27C/A1_VBackGate.png")
+
+        fig, ax = plt.subplots()
+
+        ax.plot(x, y2)
+        ax.set(xlabel='Time (s)', ylabel='VDrain (V)',
+               title='T27C-A1_VDrain')
+        ax.grid()
+        fig.savefig("../../Batchelor-Arbeit/Plots/T27C/A1_VDrain.png")
+
+        fig, ax = plt.subplots()
+
+        ax.plot(x, y3)
+        ax.set(xlabel='Time (s)', ylabel='IDrain (I)',
+               title='T27C-A1_IDrain')
+        ax.grid()
+        fig.savefig("../../Batchelor-Arbeit/Plots/T27C/A1_IDrain.png")
+        """
 
 def filesystemplotfunction():
+    path = '../../Batchelor-Arbeit/Messdaten/'  #put relative path to  here
+
+    """
+
+    for filename in os.listdir(path):       #looks for things in path
+        #print(filename)
+        if os.path.isdir(os.path.join(path,filename)):  #if thing in path is a dir print it
+            print(filename,"")
+            for filename2 in os.listdir(os.path.join(path,filename)):
+                #print(filename2)
+                if os.path.isdir(os.path.join(path, filename,filename2)):   #if thing in path is dir print it
+                    print("-",filename2)
+                    for filename3 in glob.glob(os.path.join(path,filename,filename2,"*.crv")):
+                        print("--",filename3)
+    """
+    for filenamerec in glob.iglob(os.path.join(path,"**/*.crv"),recursive=True):
+        #print(filenamerec)
+        fileplotfunction(filenamerec)
